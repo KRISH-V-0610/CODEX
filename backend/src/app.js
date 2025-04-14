448029b3487c7b1e7be83b3b07c6cf905f6088d5
KRISH0830 <166995651+KRISH0830@users.noreply.github.com>
2025-04-14 06:16:32 +0530

    redeploying backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieParser())
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://codex-frontend-roan.vercel.app/',
   credentials: true, // Enable Set-Cookie headers.
 }))
 
